f9cf21aa77e26ff334e678206896c47976815bac
bitsCode13 <laynol.vc@outlook.com>
2022-06-29 23:14:06 -0500

    CLASE APODERADO

--- src/main/java/model/Apoderado.java
+++ src/main/java/model/Apoderado.java
@@ -13,6 +13,65 @@ public class Apoderado {
 	
 	
 	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Apoderado [codApo=" + codApo + ", desApo=" + desApo + ", nomApo=" + nomApo + ", apeApo=" + apeApo
+				+ ", dniApo=" + dniApo + ", telApo=" + telApo + ", domApo=" + domApo + "]";
+	}
+	
+	
+	
+	public int getCodApo() {
+		
+		return codApo;
+	}
+	public void setCodApo(int codApo) {
+		this.codApo = codApo;
+	}
+	public String getDesApo() {
+		return desApo;
+	}
+	public void setDesApo(String desApo) {
+		this.desApo = desApo;
+	}
+	public String getNomApo() {
+		return nomApo;
+	}
+	public void setNomApo(String nomApo) {
+		this.nomApo = nomApo;
+	}
+	public String getApeApo() {
+		return apeApo;
+	}
+	public void setApeApo(String apeApo) {
+		this.apeApo = apeApo;
+	}
+	public int getDniApo() {
+		return dniApo;
+	}
+	public void setDniApo(int dniApo) {
+		this.dniApo = dniApo;
+	}
+	public int getTelApo() {
+		return telApo;
+	}
+	public void setTelApo(int telApo) {
+		this.telApo = telApo;
+	}
+	public String getDomApo() {
+		return domApo;
+	}
+	public void setDomApo(String domApo) {
+		this.domApo = domApo;
+	}
+	
+	
+	
+	
+	
 
 	
 	
